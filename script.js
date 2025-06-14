f97e3ae89ff83185f92185b22128803150cdcfd6
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-06-14 00:37:49 -0400

    fixed score counter in javascript and added a footer

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
-let rock = document.getElementById("rock");
-let paper = document.getElementById("paper");
-let scissors = document.getElementById("scissors");
 let displayResults = document.getElementById("displayResults");
+let playerScoreDisplay = document.getElementById("playerScore");
+let computerScoreDisplay = document.getElementById("computerScore");
 
 let playerScore = 0;
 let computerScore = 0;
 
 function rock() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
-    if (computerChoice === "rock") {
+    if(computerChoice === "rock") {
         document.getElementById("displayResults").innerHTML = "It's a tie!";
     } else if (computerChoice === "paper") {
-        document.getElementById("displayResults").innerHTML = "You lose!";
+        document.getElementById("displayResults").innerHTML = "You Lose!";
         computerScore++;
     } else {
-        document.getElementById("displayResults").innerHTML = "You win!";
+        document.getElementById("displayResults").innerHTML = "You Win!";
         playerScore++;
     }
+    updateScore();
 }
 
 function paper() {
     let computerChoice = Math.floor(Math.random() * 3 + 1);
     if (computerChoice === "rock") {
-        document.getElementById("displayResults").innerHTML = "You win";
+        document.getElementById("displayResults").innerHTML = "You Win!";
+        playerScore++;
     } else if (computerChoice === "paper") {
         document.getElementById("displayResults").innerHTML = "It's a tie!";
     } else {
-        document.getElementById("displayResults").innerHTML = "You lose!";
+        document.getElementById("displayResults").innerHTML = "You Lose!";
         computerScore++;
     }
+    updateScore();
 }
 
 function scissors() {
     let computerChoice = Math.floor(Math.random() * 3 + 1);
     if (computerChoice === "rock") {
-        document.getElementById("displayResults").innerHTML = "You lose!";
+        document.getElementById("displayResults").innerHTML = "You Lose!";
         computerScore++;
-    } else if (computerScore === "paper") {
-        document.getElementById("displayResults").innerHTML = "You win!";
-        playerScore++;
+    } else if(computerChoice === "paper") {
+        document.getElementById("displayResults").innerHTML = "You Win!";
     } else {
-        document.getElementById("displayResults").innerHTML = "It's a tie!";
+        document.getElementById("displayResults").innerHTML = "It's a Tie!";
     }
+    updateScore();
+}
+
+function updateScore() {
+    playerScoreDisplay.innerHTML = playerScore;
+    computerScoreDisplay.innerHTML = computerScore;
 }
