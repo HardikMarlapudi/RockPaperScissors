6d9c61bbf2e4e7fb06d9e01c12338a47bf917b13
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2026-01-01 16:46:16 -0500

    fixed gaming logic

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ let userScore = 0;
 let computerScore = 0;
 
 function play(userChoice) {
-    const choices = ["rock", "paper", "scissors"];
+    const choices = ["Rock", "Paper", "Scissors"];
     const computerChoice = choices[Math.floor(Math.random() * 3)];
-
+    
     if (userChoice === computerChoice) {
         alert("It's a tie!");
     } else if (
-        (userChoice === "rock" && computerChoice === "scissors") ||
-        (userChoice === "paper" && computerChoice === "rock") ||
-        (userChoice === "scissors" && computerChoice === "paper")
+        (userChoice === "Rock" && computerChoice === "Scissors") ||
+        (userChoice === "Paper" && computerChoice === "Rock") ||
+        (userChoice === "Scissors" && computerChoice === "Paper")
     ) {
-        userScore++;
-        document.getElementById("userScore").innerHTML = userScore;
-        alert(`You win! ${userChoice} beats ${computerChoice}.`);
+    userScore++;
+    document.getElementById("userScore").innerHTML = userScore;
+    alert(`You Win! ${userChoice} defeats ${computerChoice}.`);
     } else {
         computerScore++;
         document.getElementById("computerScore").innerHTML = computerScore;
-        alert(`Computer wins! ${computerChoice} beats ${userChoice}.`);
+        alert(`You Lose! ${computerChoice} defeats ${userChoice}.`);
     }
 }
 
-document.getElementById("rockItem").onclick = () => play("rock");
-document.getElementById("paperItem").onclick = () => play("paper");
-document.getElementById("scissorItem").onclick = () => play("scissors");
+document.getElementById("rockItem").onclick = () => play("Rock");
+document.getElementById("paperItem").onclick = () => play("Paper");
+document.getElementById("scissorItem").onclick = () => play("Scissors");
